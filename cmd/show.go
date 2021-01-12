406f516887a9e10cb2640de981ffa9a8b28ec7dd
zhangruobin001 <zhangruobin001@ke.com>
2021-01-12 16:44:12 +0800

    <U+1F4E6> NEW: open browser

--- cmd/show.go
+++ cmd/show.go
@@ -2,20 +2,50 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
+	"os/exec"
 
 	"github.com/Mrzrb/show/srv/web"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
-	file string
+	file        string
+	addr        string = "0.0.0.0:8080"
+	addrmonitor string = "0.0.0.0:8081"
 )
 
 func main() {
 	initFlag()
-	r := initWebServer()
-	r.Run("0.0.0.0:8080")
+	if file == "" {
+		flag.Usage()
+		return
+	}
+
+	if err := bootStrap(); err != nil {
+		panic("error detect")
+	}
+}
+
+func bootStrap() error {
+	var wg errgroup.Group
+
+	r, err := initWebServer()
+	if err != nil {
+		fmt.Printf("The file is not valid. Please check it")
+		return nil
+	}
+	wg.Go(func() error {
+		return r.Run(addr)
+	})
+	wg.Go(func() error {
+		cmd := exec.Command("/usr/bin/open", fmt.Sprintf("http://%s", "localhost:8080"))
+		return cmd.Start()
+	})
+	wg.Wait()
+	return nil
 }
 
 func initFlag() {
@@ -23,12 +53,12 @@ func initFlag() {
 	flag.Parse()
 }
 
-func initWebServer() *gin.Engine {
+func initWebServer() (*gin.Engine, error) {
 	r := web.DefaultRouter()
 	b, err := ioutil.ReadFile(file)
 	if err != nil {
-		panic("failed to read file" + file)
+		return nil, err
 	}
 	web.Markdown(string(b), r)
-	return r
+	return r, nil
 }
